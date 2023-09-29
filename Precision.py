b876505898956b56e63b705f02dbea095cff7e90
Greg <rlneumiller@gmail.com>
2023-09-29 19:41:18 +0100

    It works

---
+++ Precision.py
@@ -0,0 +1,59 @@
+bl_info = {
+    "name": "Configure blender for precision modeling",
+    "author": "rlneumiler@gmail.com", "various blenderheads, the interwebs"
+    "version": (1, 0),
+    "blender": (2, 80, 0),
+    "location": "View3D > UI > Tools",
+    "description": "Configure precision modeling",
+    "warning": "",
+    "doc_url": "",
+    "category": "3D View",
+}
+
+import os
+import bpy
+import bpy.utils.previews
+    
+class PRECISION_OT_func(bpy.types.Operator):
+    bl_idname = "precision.func"
+    bl_label = "Set mm precision"
+
+    def execute(self, context):
+        bpy.context.scene.unit_settings.scale_length = 0.001
+        bpy.context.scene.unit_settings.length_unit = 'MILLIMETERS'
+        bpy.context.scene.unit_settings.mass_unit = 'GRAMS'
+     
+        space = bpy.context.space_data
+        space.overlay.show_stats=True
+        space.clip_end = 10000
+        space.clip_start = 0.01   
+        
+        return {'FINISHED'}
+    
+class CUSTOM_PT_myPanel(bpy.types.Panel):
+    bl_label = "Configure Precision"
+    bl_idname = "OBJECT_PT_custom_panel"
+    bl_space_type = "VIEW_3D"   
+    bl_region_type = "UI"
+    bl_category = "Precision"
+
+    def draw(self, context):
+        global custom_icons
+        layout = self.layout
+        layout.operator(PRECISION_OT_func.bl_idname)
+        self.layout.label(text="custom icon test", icon_value=custom_icons["custom_icon"].icon_id)
+
+
+
+classes = (PRECISION_OT_func, CUSTOM_PT_myPanel)
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+def unregister():
+    bpy.utils.unregister_class(classes)
+
+if __name__ == "__main__":
+    register()
+    
+    
